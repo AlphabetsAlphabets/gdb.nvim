70e494390a1a2d5f500721e1cd19e46998523359
Yap Jia Hong <yapjiahong2003@gmail.com>
2023-02-21 11:50:59 +0800

    `setup` function now works. User can override default settings. Modularized code as well.

--- lua/gdb/init.lua
+++ lua/gdb/init.lua
@@ -2,44 +2,43 @@
 
 local M = {}
 
-M.options = {
+M.default_options = {
 	-- `boolean`. If `true` then asking for source file will not be asked.
 	default_to_buf = true,
-
 	-- `boolean`. If `true` then asking for binary file will not be asked.
 	detect_binary = true,
 }
 
--- This function doesn't actually do anything.
+-- User opts will override default options.
+-- Any value not passed in will retain its default values
 M.setup = function(user_opts)
-	for k, v in pairs(user_opts) do
-		M.options[k] = v
-	end
+	M.default_options = vim.tbl_extend("force", M.default_options, user_opts)
 end
 
-local function create_prompts()
-	local file = nil
-	local binary = nil
-	local completion = "file"
-	local prompt = ""
+local function prompt(question)
+	local input = vim.fn.input(question, '', "file")
+	return input
+end
 
-	if M.options.default_to_buf == false then
-		prompt = "Name of source file (defaults to current buffer if empty): "
-		file = vim.fn.input(prompt, '', completion)
+-- TODO: Implement this
+local function find_project_root()
+end
+
+local function setup_workspace()
+	local file = nil
+	if M.default_options.default_to_buf == false then
+		file = prompt("Name of source file (defaults to current buffer if empty): ")
 	else
 		file = vim.fn.expand("%:p")
 	end
 
-	if M.options.detect_binary == false then
-		prompt = "Name of binary to debug: "
-		binary = vim.fn.input(prompt, '', completion)
+	-- Need to find out a way to detect the binary.
+	local binary = ''
+	if M.default_options.detect_binary == false then
+		binary = prompt("Name of binary to debug: ")
+	else
+		-- TODO: Auto find binary
 	end
-
-	return file, binary
-end
-
-local function create_splits()
-	local file, binary = create_prompts()
 
 	-- Creates a new tab with the source file
 	vim.cmd("tabnew " .. file)
@@ -58,10 +57,11 @@ local function create_splits()
 
 	-- `term_win` is the window from splitting the screen
 	local term_win = vim.api.nvim_get_current_win()
+
 	-- I have no idea why this is reversed
 	if width < MIN_WIDTH then
 		-- Handle vsplit stuff here
-		local size = math.floor(width * 0.20);
+		local size = math.floor(height * 0.50);
 		vim.api.nvim_win_set_height(term_win, size)
 	else
 		-- Handle split here
@@ -86,7 +86,7 @@ local function create_splits()
 end
 
 M.debug = function()
-	create_splits()
+	setup_workspace()
 end
 
 return M
--- plugin/gdb.vim
+++
@@ -1,3 +0,0 @@
-" This value doesn't get overwritten if the user calls it
-" luado require("gdb").setup({detect_binary = false}) 
-command! Debug lua require("gdb").debug()
